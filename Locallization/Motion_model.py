1ecad7cb0d67b8361f00c2fe1dcb64da5574d679
AyushamMishra <mishraayusham5@gmail.com>
2026-01-25 14:27:40 +0530

    2D motion model changes

--- Locallization/Motion_model.py
+++ Locallization/Motion_model.py
@@ -15,35 +15,48 @@ dt = time interval between steps
 """
 
 
+"""
+Motion Model
+State      : [x, y, yaw]
+Control    : [v, omega]
+Units      : meters, radians, seconds
+"""
+
 import numpy as np
-sigma_v = 0.5       #  velocity noise
-sigma_yaw =0.01     # yaw noise
 
 class MotionModel:
-    def __init__(self, sigma_v, sigma_yaw):
+    def __init__(self, sigma_v=0.5, sigma_yaw=0.01, sigma_omega=0.01):
         self.sigma_v = sigma_v
         self.sigma_yaw = sigma_yaw
+        self.sigma_omega = sigma_omega
 
-    def propogate(self,state,control_inputs,dt):
+    def propagate(self, state, control_inputs, dt, add_noise=True):
 
-        v,yaw_rate= control_inputs
+        v_cmd, omega_cmd = control_inputs
+        N = state.shape[0]
 
-        N=state.shape[0]  # number of particles
+        state_new = state.copy()
+        yaw = state[:, 2]
 
-        ## Adding noise to control inputs
-        v_noise= np.random.normal(0,self.sigma_v,N)
-        yaw_noise= np.random.normal(0,self.sigma_yaw,N)
+        if add_noise:
+            v_noise = np.random.normal(0, self.sigma_v, N)
+            omega_noise = np.random.normal(0, self.sigma_omega, N)
+            yaw_noise = np.random.normal(0, self.sigma_yaw, N)
+        else:
+            v_noise = 0.0
+            omega_noise = 0.0
+            yaw_noise = 0.0
 
-        v_eff=v+v_noise
-        yaw=state[:,2]
+        # Effective controls
+        v_eff = v_cmd + v_noise
+        omega_eff = omega_cmd + omega_noise
 
-        ## Kinematic updates for (x,y) coordinates
+        # Kinematic propagation
+        state_new[:, 0] += v_eff * np.cos(yaw) * dt
+        state_new[:, 1] += v_eff * np.sin(yaw) * dt
+        state_new[:, 2] += omega_eff * dt + yaw_noise
 
-        state[:,0] += v_eff * np.cos(yaw) * dt
-        state[:,1] += v_eff * np.sin(yaw) * dt
-        state[:,2] += yaw_rate * dt + yaw_noise
+        # Normalize yaw to [-pi, pi]
+        state_new[:, 2] = (state_new[:, 2] + np.pi) % (2 * np.pi) - np.pi
 
-        ## Normalize yaw to [-pi, pi]
-        state[:,2] = (state[:,2] + np.pi) % (2 * np.pi) - np.pi
-
-        return state
+        return state_new
