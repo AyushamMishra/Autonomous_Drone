27f5b4489d39a965c7f3dfc001216eb0ccccb5ad
AyushamMishra <mishraayusham5@gmail.com>
2026-02-22 13:05:33 +0530

    Refactor MotionPrimitives class to simplify motion generation; update constructor and primitives method for clarity and efficiency.

--- Path_planning/motion_primitives.py
+++ Path_planning/motion_primitives.py
@@ -1,22 +1,27 @@
-from sre_parse import State
+from Path_planning.state import State
 import numpy as np
 
 class MotionPrimitives:
-    def __init__(self, step=2.5, max_steer=np.deg2rad(30)):
+    def __init__(self, step=1.0, max_steer=np.deg2rad(30), num_yaws=16):
         self.step = step
+        self.num_yaws = num_yaws
+
         self.steers = [-max_steer, -max_steer/2, 0.0, max_steer/2, max_steer]
+        self.yaw_res = 2*np.pi / num_yaws
 
-    def primitives(self,s):
+    def primitives(self):
         """
-        dx, dy, dyaw (dy relative to heading frame)
+        Returns list of (dx, dy, dyaw_index)
+        Motion in local vehicle frame
         """
-        states = []
+        motions = []
+
         for delta in self.steers:
-            yaw = s.yaw + delta
-            x = s.x + self.step * np.cos(yaw)
-            y = s.y + self.step * np.sin(yaw)
+            dx = self.step
+            dy = 0.0
 
-            nxt = State(x, y, yaw)
-            nxt.g = s.g + self.step
-            states.append(nxt)
-        return states
+            dyaw_index = int(round(delta / self.yaw_res))
+
+            motions.append((dx, dy, dyaw_index))
+
+        return motions
